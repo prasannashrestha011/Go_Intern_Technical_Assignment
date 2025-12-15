793267491d212cfc53e21da866f9b1f38508d5ea
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-15 18:13:00 +0545

    Add migration for creating users table with pgcrypto extension

---
+++ migrations/20251215121424_create_users_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT,
+    email TEXT UNIQUE,
+    password TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+)
+-- +goose StatementEnd
+
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+ DROP TABLE IF EXISTS users
+-- +goose StatementEnd
