d9ca1aef551b84d12524b25b97c58204a2df105a
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-16 21:01:40 +0545

    add: refactor user model and DTOs to use uuid for ID and enhance data structure

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,11 +1,49 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+
 
 type User struct {
-	ID         uint64
+	ID         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Name       string
 	Email      string
 	Password   string
 	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (User) TableName() string{
+	return `"user"`
+}
+//DTOs
+
+type UserCreateDTO struct{
+	Name 	   string `json:"name" binding:"required"`
+	Email      string `json:"email" binding:"required,email"`
+	Password   string `json:"password" binding:"required,min=6"`
+}
+
+
+type UserLoginDTO struct{
+	Email string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserUpdateDTO struct{
+	Name 	   string `json:"name" binding:"required"`
+	Email	   string `json:"email" binding:"required,email"`
+	Password   string `json:"password" binding:"required"`
+}
+
+type UserResponseDTO struct{
+	ID 		  uuid.UUID `json:"id"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
