f4c66d7ae0a349391ddacb049071a7d861cc7cf9
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-12-16 21:01:51 +0545

    refactor: update UserRepository to use uuid.UUID for ID and enhance method signatures

--- internal/repository/userRepository.go
+++ internal/repository/userRepository.go
@@ -3,15 +3,17 @@ package repository
 import (
 	"main/internal/models"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type UserRepository interface {
 	Create(user *models.User) error
-	GetByID(id int64) (*models.User, error)
+	GetByID(id uuid.UUID) (*models.User, error)
 	GetByEmail(email string) (*models.User, error)
-	Update(user *models.User)(*models.User,error) 
-	Delete(id int64) error
+	GetAll() ([]*models.User, error)
+	Update(user *models.User) (*models.User, error)
+	Delete(id uuid.UUID) error
 }
 
 type userRepo struct {
@@ -23,34 +25,42 @@ func NewRepository(db *gorm.DB) UserRepository {
 	return &userRepo{db: db}
 }
 
-
 func (r *userRepo) Create(user *models.User) error {
 	return r.db.Create(user).Error
 }
 
-func (r *userRepo) Delete(id int64) error {
-	return r.db.Delete(&models.User{},id).Error
+func (r *userRepo) Delete(id uuid.UUID) error {
+	return r.db.Delete(&models.User{}, id).Error
+}
+
+// pagination will be implemented in the future
+func (r *userRepo) GetAll() ([]*models.User, error) {
+	var users []*models.User
+	if err:=r.db.Find(users).Error;err!=nil{
+		return nil,err
+	}
+	return users,nil
 }
 
 func (r *userRepo) GetByEmail(email string) (*models.User, error) {
 	var user models.User
-	if err:=r.db.Where("email= ?",email).First(&user).Error;err!=nil{
-		return nil,err
+	if err := r.db.Where("email= ?", email).First(&user).Error; err != nil {
+		return nil, err
 	}
-	return &user,nil
+	return &user, nil
 }
 
-func (r *userRepo) GetByID(id int64) (*models.User, error) {
-	var user models.User 
-	if err:=r.db.Where("id= ?",id).First(&user).Error;err!=nil{
-		return nil,err
+func (r *userRepo) GetByID(id uuid.UUID) (*models.User, error) {
+	var user models.User
+	if err := r.db.Where("id= ?", id).First(&user).Error; err != nil {
+		return nil, err
 	}
-	return &user,nil
+	return &user, nil
 }
 
-func (r *userRepo) Update(user *models.User) (*models.User,error) {
-	if err:=r.db.Save(&user).Error;err!=nil{
-		return nil,err
+func (r *userRepo) Update(user *models.User) (*models.User, error) {
+	if err := r.db.Save(user).Error; err != nil {
+		return nil, err
 	}
-	return user,nil
+	return user, nil
 }
